04c0d20a6bdee9977caa94c28798159ae419edbd
Nikolas Martens <Nikolas.M@rtens.org>
2024-09-04 21:57:06 +0200

    Init for Windows

--- .emacs-local.el
+++ .emacs-local.el
@@ -2,7 +2,16 @@
           (lambda ()
             (split-window-horizontally -25)
 	    (other-window 1)
-            (term "./venv/bin/ptw -- -q")
+	    (if (eq system-type 'windows-nt)
+		(progn
+		  (eshell)
+		  (with-current-buffer "*eshell*"
+		    (visual-line-mode)
+		    (eshell-return-to-prompt)
+		    (insert "venv/Scripts/ptw.exe -- -q")
+		    (eshell-send-input)))
+	      (term "./venv/bin/ptw -- -q")
+	      )
 	    (other-window 1)
 	    )
 	  )
